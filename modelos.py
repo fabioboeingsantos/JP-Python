149d4926b183a6ccca92ab0b92b429509ad00034
Fabio Boeing Santos <fabio.boeing@yahoo.com.br>
2026-06-22 21:46:29 -0300

    001

--- modelos.py
+++ modelos.py
@@ -4,17 +4,13 @@ class Alunos:
     def __init__(self, nome, e_mail, id_alunos, media):
         self.id = id
         self.nome = nome
-        self.e_mail = e_mail
-        self.id_alunos = id_alunos
         self.media = media
        
     def exibir(self):
         texto = f"""
-        Código: {self.id}
+        id: {self.id}
         Nome: {self.nome}
-        Preco: {self.e_mail}
-        Categoria: {self.id_alunos}
-        Ativo: {self.media}
+        media: {self.media}
         """
         print(texto)
 
